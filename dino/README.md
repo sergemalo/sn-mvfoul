ec297d56609c1f35bf0a174e2a62dc55eb0a089f
Edouard Demotes <e.demotes@gmail.com>
2025-03-10 07:20:14 -0400

    chore: cleanup and readme

---
+++ dino/README.md
@@ -0,0 +1,15 @@
+# DINOv2 Depther
+
+## Usage
+
+Build the docker image:
+
+```bash
+docker build -t dino-depther .
+```
+
+Run the docker container:
+
+```bash
+docker run --gpus all dino-depther
+```
--- dino/main.py
+++ dino/main.py
@@ -13,6 +13,6 @@ if __name__ == "__main__":
     image = load_image_from_url("https://dl.fbaipublicfiles.com/dinov2/images/example.jpg")
     model = Depther()
     depth_image = model.get_depth_for_image(image)
-    depth_image.save("/output/depth_image.png")
+    depth_image.save("/app/output/depth_image.png")
 
 
